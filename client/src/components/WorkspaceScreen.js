3902120f6f9cb499a4648dea95a2fb5ec388256f
Derek Wang <toderekwang@gmail.com>
2022-03-30 15:53:02 -0400

    workspace quill i think should work

--- client/src/components/WorkspaceScreen.js
+++ client/src/components/WorkspaceScreen.js
@@ -1,9 +1,25 @@
-import {useContext} from 'react'
+import { useContext } from 'react'
+import { GlobalStoreContext } from '../store'
+import Quill from 'quill/core'
+
 
 function WorkspaceScreen() {
+    const { store } = useContext(GlobalStoreContext);
+
+    var quill = new Quill('#editor-container', {
+        modules: {
+            toolbar: [
+                ['bold', 'italic', 'underline'],
+                ['image']
+            ]
+        },
+        placeholder: 'The loathsome Dung Eater',
+        theme: 'snow'
+    })
+
     return (
         <h1>
-            Workspace Screen
+            return quill
         </h1>
     )
 }
