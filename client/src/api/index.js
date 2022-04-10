af2c7215d7564702958147bd41904f6e8fab0cc1
Bennie Chen <Bennie.chen09@gmail.com>
2022-04-10 19:05:08 -0400

    Connect Backend to Frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,8 +6,7 @@ const api = axios.create({
     baseURL: 'http://' + ip + ':4000/'
 })
 
-// Server Connections
-
+/* Text Editing */
 export const connect = (id) => api.get('/connect/'+id)
 .then(response => {
     return response
@@ -25,12 +24,39 @@ export const operation = (id, payload) => api.post('/op/'+id, payload)
 export const getdoc = (id) => api.get('/doc/'+id)
 export const getAllDocs = () => api.get('/alldoc')
 
+/* Authentication */
+export const addUser = (payload ) => api.post(`/adduser`, payload)
+.then(response => {
+    return response
+ })
+ .catch(error => {
+    return error.response;
+ });
+export const loginUser = (payload) => api.post(`/login`, payload)
+.then(response => {
+    return response
+ })
+ .catch(error => {
+    return error.response
+ });
+export const logoutUser = (payload) => api.post(`/logout`, payload)
+.then(response => {
+    return response
+ })
+ .catch(error => {
+    return error.response
+ });
+
+
 
 const apis = {
     connect,
     operation,
     getdoc,
-    getAllDocs
+    getAllDocs,
+    addUser,
+    loginUser,
+    logoutUser
 }
 
 export default apis
--- server/routes/text-router.js
+++ server/routes/text-router.js
@@ -3,12 +3,17 @@ const textController = require('../controller/text-controller')
 const UserController = require('../controller/user-controller')
 const router = express.Router()
 
-// Game
+// Text Editing
 router.get('/connect/:id', textController.connect)
 router.get('/connect', textController.rawConnect)
 router.post('/op/:id', textController.operation)
 router.get('/doc/:id', textController.getdoc)
 router.get('/alldoc', textController.alldoc)
+
+// User Authentication
+router.post('/adduser', UserController.registerUser)
+router.post('/login', UserController.loginUser)
+router.post('/logout', UserController.logoutUser)
 
 
 module.exports = router
