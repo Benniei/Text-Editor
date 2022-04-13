a7dc6d584f7a4719afc5968e5493526ef329365a
Bennie Chen <Bennie.chen09@gmail.com>
2022-04-12 23:42:06 -0400

    Authenticated  routers - to use on cloud

---
+++ router-secure.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const auth = require('../auth')
+const UserController = require('../controller/user-controller')
+const CollectionController = require('../controller/collection-controller')
+const MediaController = require('../controller/media-controller')
+const DocController = require('../controller/doc-controller')
+
+const router = express.Router()
+
+// User Authentication
+router.post('/users/signup', UserController.registerUser)
+router.post('/users/login', UserController.loginUser)
+router.post('/users/logout', UserController.logoutUser)
+router.get('/users/verify', UserController.verifyUser)
+router.get('/user/loggedIn', UserController.userLoggedIn)
+
+// Collection Creation
+router.post('/collection/create', auth.verify, CollectionController.createCollection)
+router.post('/collection/delete', auth.verify, CollectionController.deleteCollection)
+router.get('/collection/list', auth.verify, CollectionController.listCollection)
+
+// Media Controller
+router.post('/media/upload', auth.verify, MediaController.uploadMedia)
+router.get('/media/access/:id', auth.verify, MediaController.accessMedia)
+
+// Document Editing
+router.get('/doc/connect/:docid/:uid', auth.verify, DocController.connect)
+router.post('/doc/op/:docid/:uid', auth.verify, DocController.operation)
+router.get('/doc/get/:docid/:uid', auth.verify, DocController.getdoc)
+router.post('/doc/presence/:docid/:uid', auth.verify, DocController.presence)
+
+
+
+
+
+module.exports = router
