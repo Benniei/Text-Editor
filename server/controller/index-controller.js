330e333ec9d15f5873bb7de5b83f5c5f000a3d9a
hadalittlelamb <37488301+hLamb@users.noreply.github.com>
2022-04-26 01:17:19 -0400

    snippet update

--- server/controller/index-controller.js
+++ server/controller/index-controller.js
@@ -4,13 +4,20 @@ search = async (req, res) => {
     const searchRes = await Text.search({
         query_string: {
             query: "Albert is simple"
+        },
+        "highlight": {
+            "number_of_fragments" : 1,
+            "fragment_size" : 100,
+            "fields" : {
+              "body" : { "pre_tags" : ["<em>"], "post_tags" : ["</em>"] }
+            }
         }
     })
     results = searchRes.body.hits.hits
     var finalResult = []
     for(var i = 0; i < Math.min(10, results.length); i++){
         const item = results[i]._source
-        const finalString = "<em>" + item.content + "</em>"
+        const finalString = result[i].highlight.content;
         var data = {
             docid: item.id,
             name: item.name,
