a0877ee28d282417271c04565fdfa38d0fdc7319
Bennie Chen <Bennie.chen09@gmail.com>
2022-04-29 17:56:11 -0400

    elsatic serach functional

--- server/controller/index-controller.js
+++ server/controller/index-controller.js
@@ -4,7 +4,7 @@ const url = require('url')
 search = async (req, res) => {
     console.log("------------search")
     var queryContent = url.parse(req.url, true).query.q;
-    const searchRes = await ElasticClient.search({
+    await ElasticClient.search({
         index: 'texts',
         query: {
             match: {content: queryContent}
@@ -16,20 +16,26 @@ search = async (req, res) => {
                 content: {}
             }
         }
-    })
-    results = searchRes.hits.hits
-    var finalResult = []
-    for(var i = 0; i < Math.min(10, results.length); i++){
-        const item = results[i]._source
-        const finalString = results[i].highlight;
-        var data = {
-            docid: item.docid,
-            name: item.name,
-            snippet: finalString //patch up later
+    }).then(
+        function(searchRes) {
+            results = searchRes.hits.hits
+            var finalResult = []
+            for(var i = 0; i < Math.min(10, results.length); i++){
+                const item = results[i]._source
+                const finalString = results[i].highlight;
+                var data = {
+                    docid: item.docid,
+                    name: item.name,
+                    snippet: finalString //patch up later
+                }
+                finalResult.push(data)
+            }
+            res.status(200).json(finalResult).end()
+        },
+        function(err) {
+            console.trace(err.message);
         }
-        finalResult.push(data)
-    }
-    res.status(200).json(finalResult).end()
+    )
 }
 
 suggest = async (req, res) => {
