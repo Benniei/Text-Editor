a975aeda135c939be7820ca9aedc3d8c6bbd976d
Bennie Chen <Bennie.chen09@gmail.com>
2022-05-05 03:05:15 -0400

    snippet

--- server/controller/index-controller.js
+++ server/controller/index-controller.js
@@ -55,7 +55,7 @@ search = async (req, res) => {
             finalResult.push(data)
         }
         console.log(finalResult)
-        finalResults.sort((a, b) => {(a.match(/<em>/g) || []).length - (a.match(/<em>/g) || []).length})
+        finalResult.sort((a, b) => {(a.snippet.match(/<em>/g) || []).length - (b.snippet.match(/<em>/g) || []).length})
         searchCache[queryContent] = finalResult
         res.status(200).json(finalResult).end()
     }
